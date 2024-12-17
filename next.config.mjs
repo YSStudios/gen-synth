a49371c364cd67122be11decd7532a8eb88371de
Kirill Ginko <53977030+kirillginko@users.noreply.github.com>
2024-12-16 21:04:45 -0500

    updated phone styling

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: "../.next",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "/images/yqk7lu4g/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/ddkuxrisq/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
