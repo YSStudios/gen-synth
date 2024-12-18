155b2a2f092b6169270d5bd734b9773d09df9741
Kirill Ginko <53977030+kirillginko@users.noreply.github.com>
2024-12-17 22:59:10 -0500

    added meta data

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
+import Head from "next/head";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Video from "./components/Video";
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Gen Synth</title>
+        <meta name="description" content="Gen Synth" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main>
         <Video />
       </main>
